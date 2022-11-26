ce0bca70145bdd4831e124a8cb24914fd6875e62
Marchejd <115989035+Marchejd@users.noreply.github.com>
2022-11-26 18:30:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+    int a=1, b=1, c=0, n=0;
+    cin>>n;
+cout<<a<<endl;
+cout<<b<<endl;
+while (c<=n)
+{
+            c = a + b;
+          if (c<=n)  {cout << c << endl;}
+            a = b;
+            b = c;
+
+        }
    return 0;
 }
